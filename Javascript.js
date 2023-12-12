7f59817a949d6204207ce5c70a69fac36df578e0
Divyabharathi Gopi <104125342+DivyabharathiGopi@users.noreply.github.com>
2023-12-12 21:55:49 +0530

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -1,1 +1,14 @@
+   
+        const menu = document.querySelector('.menu')
+        const menuList = document.querySelector('nav ul')
+        menu.addEventListener('click',()=>{
+            menuList.classList.toggle('showmenu')
+        });
 
+        const skillimg=document.querySelector('.education')
+        const skillpara=document.querySelector('p')
+        education.addEventListener('click',()=>{
+            menuList.classList.toggle('#skillpara')
+        });
+
+  
